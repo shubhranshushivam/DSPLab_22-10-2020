d9aa561d9c3a5bf3f2b365939526f0ab7a3ef782
shubhranshushivam <52876165+shubhranshushivam@users.noreply.github.com>
2020-10-22 10:17:41 +0530

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -1,15 +1,15 @@
 # 2. write a program to find the reverse of a given number without any loops.
 
 
-# x=(input("Enter a number="))
-# print(int(x[::-1]))
+x=(input("Enter a number="))
+print(int(x[::-1]))
 
-def reverse(x):
-    res=''
-    for i in range(len(x)-1,-1,-1):
-        res+=x[i]
+# def reverse(x):
+#     res=''
+#     for i in range(len(x)-1,-1,-1):
+#         res+=x[i]
 
-    return res
+#     return res
 
-x=int(input("ENTER A NUMBER="))
-print(int(reverse(str(x))))
+# x=int(input("ENTER A NUMBER="))
+# print(int(reverse(str(x))))
